3de225795a98c5da35b80d74a06f279d838f4e70
Philpax <me@philpax.me>
2015-10-10 11:31:48 +1100

    Fix bug where EBX was being trampled in cpuid_get_vendor_id_string

--- src/cpuid.s
+++ src/cpuid.s
@@ -1,9 +1,11 @@
 global cpuid_get_vendor_id_string
 cpuid_get_vendor_id_string:
 	mov eax, 0
+	push ebx
 	cpuid
-	mov eax, [esp+4]
+	mov eax, [esp+8]
 	mov [eax], ebx
 	mov [eax+4], edx
 	mov [eax+8], ecx
+	pop ebx
 	ret
